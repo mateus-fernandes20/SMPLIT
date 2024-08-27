98e8655012be8a0f0eda9a27c9b6c8c9ded0c996
Mateus Fernandes <140284146+mateus-fernandes20@users.noreply.github.com>
2024-08-27 18:19:43 -0300

    Create AudioFile.java

---
+++ AudioFile.java
@@ -0,0 +1,48 @@
+public class AudioFile {
+    private int id;
+    private String fileName;
+    private String tempo;
+    private String genre;
+    private String description;
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getTempo() {
+        return tempo;
+    }
+
+    public void setTempo(String tempo) {
+        this.tempo = tempo;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
